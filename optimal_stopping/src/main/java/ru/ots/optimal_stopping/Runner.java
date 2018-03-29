0e5d5fdce47d95918af3e175d78858b5c327cb60
Ярослав <jaroslav@Aroslavs-MacBook-Pro.local>
2018-03-29 07:30:33 +0300

    jj

--- optimal_stopping/src/main/java/ru/ots/optimal_stopping/Runner.java
+++ optimal_stopping/src/main/java/ru/ots/optimal_stopping/Runner.java
@@ -1,12 +1,16 @@
 package ru.ots.optimal_stopping;
 
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.population.*;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy;
 import org.matsim.core.replanning.GenericStrategyManager;
 import org.matsim.core.scenario.ScenarioUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Runner {
 
@@ -23,6 +27,28 @@ public class Runner {
 
 
         Scenario scenario = ScenarioUtils.loadScenario(config) ;
+
+        List<Person> personList = new ArrayList<>(scenario.getPopulation().getPersons().values());
+
+        for (Person person : personList) {
+            Plan selectedPlan = person.getSelectedPlan();
+            person.setSelectedPlan(null);
+            for (Plan plan : new ArrayList<>(person.getPlans())) {
+                person.removePlan(plan);
+            }
+            ArrayList<PlanElement> allPlanElements = new ArrayList<>(selectedPlan.getPlanElements());
+            selectedPlan.getPlanElements().clear();
+            Plan newPlan = scenario.getPopulation().getFactory().createPlan();
+            newPlan.setPerson(person);
+            for (PlanElement allPlanElement : allPlanElements) {
+                if (allPlanElement instanceof Leg){
+                    newPlan.addLeg((Leg) allPlanElement);
+                } else if (allPlanElement instanceof Activity){
+                    newPlan.addActivity((Activity) allPlanElement);
+                }
+            }
+            person.addPlan(newPlan);
+        }
 
         Controler controler = new Controler( scenario ) ;
 
